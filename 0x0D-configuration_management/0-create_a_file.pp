680cca3f599eaa068d17e7c9632be3f12a4a1b57
Surfskills <ojjfred@gmail.com>
2022-09-16 20:56:42 +0300

    Configuration management

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-# create a file in /tmp with specific requirements
+# Create a file in /tmp
 
 file { 'holberton':
-  path    => '/tmp/holberton',
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+  path    => '/tmp/holberton',
 }
